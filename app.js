803bbced3267d6c1fcb133073c8deebb7d81f033
Bazsa241 <wojtek011@windowslive.com>
2020-10-12 19:09:39 +0200

    BookApp

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const input = document.querySelector("#input");
+const searchBtn = document.querySelector("#search-btn");
+const main = document.querySelector("main");
+
+searchBtn.addEventListener("click", event => {
+  console.log(event);
+  // getBooks(input.value)
+});
+
+document.addEventListener("keypress", event => {
+  if(event.key === "Enter") {
+      getBooks(input.value)
+  }
+});
+
+function getBooks(search, maxResult = 10, startIndex = 0) {
+  main.innerHTML = "";
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=
+  ${search}&maxResults=${maxResult}&startIndex=${startIndex}`)
+  .then(response => response.json())
+  .then(result => {
+    for (let i = 0; i < result.items.length; i++) {
+      renderBooks(result.items[i].volumeInfo.title,
+                  result.items[i].volumeInfo.authors,
+                  result.items[i].volumeInfo.imageLinks.thumbnail
+                  );
+    }
+  })
+};
+
+function renderBooks(title, author, thumbnail) {
+  main.innerHTML +=
+   `<div class="card">
+      <img src="${thumbnail}" alt="thumbnail">
+      <div class="info">
+        <h3>${title}</h3>
+        <p>by ${author}</p>
+        <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+        <i class="far fa-star"></i>
+        </div>
+      </div>
+    </div>`
+}
